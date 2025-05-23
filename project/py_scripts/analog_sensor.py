2d97eccffcfb6c3441c60f22640e90a6cde52c75
thatoneartkid <zoe.rath@education.nsw.gov.au>
2025-05-23 12:49:52 +1000

    woe, sensors be upon ye

--- project/py_scripts/analog_sensor.py
+++ project/py_scripts/analog_sensor.py
@@ -6,14 +6,17 @@ sleep(0.1)
 led_pin = 25
 led2_pin = 15
 data_pin = 13
+analog_data_pin = 26
 
 led = Pin(led_pin, Pin.OUT)
 led2 = Pin(led2_pin, Pin.OUT)
 
 data = Pin(data_pin, Pin.IN)
 
+adc_value = ADC(Pin(analog_data_pin))
+
 while True:
-    if data.value() == 1:
+    if data_value == 1:
         led.value(True)
         led2.value(False)
     else:
